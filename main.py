928751a05c997067e5dbaa5bbf154b5eedf0313f
Sudhindra Deshpande <sudhiinlook@gmail.com>
2026-07-12 10:42:46 +0530

    This is the first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
